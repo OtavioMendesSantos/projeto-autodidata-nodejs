0c206e8c2e6aa25a09a345b787cc751979cee7ef
Otávio <otaviomendessantos2019@gmail.com>
2026-07-28 18:18:48 -0300

    course: 0202 Request 2 - chunks

--- Origamid/02-servidor-simples/client.mjs
+++ Origamid/02-servidor-simples/client.mjs
@@ -1,12 +1,17 @@
-const get = await fetch('http://localhost:3000/produtos?cor=azul&tamanho=g', { method: 'GET' });
-console.log(await get.text());
+const post = await fetch('http://localhost:3000/login', {
+  method: 'POST',
+  headers: { 'content-type': 'application/json' },
+  body: JSON.stringify({
+    name: 'Otávio',
+    email: 'otavio@gmail.com',
+    senha: '123123',
+  }),
+});
+console.log(await post.text());
 
-// const post = await fetch('http://localhost:3000/produtos', {
-//   method: 'POST',
-//   headers: { 'content-type': 'application/json' },
-// });
-// console.log(await post.text());
-
+// const get = await fetch('http://localhost:3000/produtos?cor=azul&tamanho=g', { method: 'GET' });
+// console.log(await get.text());
+//
 // const response = await fetch('http://localhost:3000/', { method: 'GET' });
 // console.log(await response.text());
-// 
+
--- Origamid/02-servidor-simples/server.mjs
+++ Origamid/02-servidor-simples/server.mjs
@@ -2,14 +2,35 @@ import { createServer } from 'node:http';
 
 const PORT = 3000;
 
-const server = createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  // res.setHeader('Content-Type', 'text/plain; charset=utf-8'); 
+// const frase1 = Promise.resolve('Ola ');
+// const frase2 = Promise.resolve('Mundo');
+// const frase3 = Promise.resolve('!');
+// const frasesPromisses = [frase1, frase2, frase3];
+// const frases = [];
+// for await (const frase of frasesPromisses) {
+//   frases.push(frase);
+// }
+// console.log(frases.join(''));
+
+// const parte1 = Buffer.from('Olá ');
+// const parte2 = Buffer.from('Mundo');
+// const final = Buffer.concat([parte1, parte2]).toString('utf-8');
+// console.log(final);
+
+const server = createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
 
   const url = new URL(req.url || '/', 'http://localhost:' + PORT);
+  const chunks = [];
+  for await (const chunk of req) {
+    chunks.push(chunk);
+  }
+  const body = Buffer.concat(chunks).toString("utf-8")
+  
   console.log('[Request received]', req.method, url.pathname);
   console.log('[Request headers]', req.headers);
-
+  console.log('[Request body]', JSON.parse(body));
+  
   if (req.method === 'GET' && url.pathname === '/produtos') {
     res.statusCode = 200;
 
@@ -21,6 +42,9 @@ const server = createServer((req, res) => {
   } else if (req.method === 'POST' && url.pathname === '/produtos') {
     res.statusCode = 201;
     res.end('POST!');
+  } else if (req.method === 'POST' && url.pathname === '/login') {
+    res.statusCode = 200;
+    res.end('LOGIN!');
   } else {
     res.statusCode = 404;
     res.end('Página não encontrada!');
