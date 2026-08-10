35bb9d5f98b61d10d654a75b2318be3164ecdf0a
Otávio <otaviomendessantos2019@gmail.com>
2026-08-10 18:27:21 -0300

    course: 0203 Response

--- Origamid/02-servidor-simples/client.mjs
+++ Origamid/02-servidor-simples/client.mjs
@@ -1,17 +1,22 @@
-const post = await fetch('http://localhost:3000/login', {
-  method: 'POST',
-  headers: { 'content-type': 'application/json' },
-  body: JSON.stringify({
-    name: 'Otávio',
-    email: 'otavio@gmail.com',
-    senha: '123123',
-  }),
-});
-console.log(await post.text());
-
-// const get = await fetch('http://localhost:3000/produtos?cor=azul&tamanho=g', { method: 'GET' });
-// console.log(await get.text());
-//
-// const response = await fetch('http://localhost:3000/', { method: 'GET' });
+// const response = await fetch('http://localhost:3000/login', {
+//   method: 'POST',
+//   headers: { 'content-type': 'application/json' },
+//   body: JSON.stringify({
+//     name: 'Otávio',
+//     email: 'otavio@gmail.com',
+//     senha: '123123',
+//   }),
+// });
+// console.log(response)
 // console.log(await response.text());
 
+// const response = await fetch('http://localhost:3000/produtos?cor=azul&tamanho=g', { method: 'GET' });
+// console.log(response.headers)
+// console.log(typeof await response.text()); // String
+// console.log(typeof await response.json()); // Object
+
+const response = await fetch('http://localhost:3000/', { method: 'GET' });
+console.log(await response.text())
+
+// const response = await fetch('http://localhost:3000/', { method: 'GET' });
+// console.log(await response.text());
--- Origamid/02-servidor-simples/server.mjs
+++ Origamid/02-servidor-simples/server.mjs
@@ -2,48 +2,72 @@ import { createServer } from 'node:http';
 
 const PORT = 3000;
 
-// const frase1 = Promise.resolve('Ola ');
-// const frase2 = Promise.resolve('Mundo');
-// const frase3 = Promise.resolve('!');
-// const frasesPromisses = [frase1, frase2, frase3];
-// const frases = [];
-// for await (const frase of frasesPromisses) {
-//   frases.push(frase);
-// }
-// console.log(frases.join(''));
-
-// const parte1 = Buffer.from('Olá ');
-// const parte2 = Buffer.from('Mundo');
-// const final = Buffer.concat([parte1, parte2]).toString('utf-8');
-// console.log(final);
-
 const server = createServer(async (req, res) => {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+  // CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Cache
+  // res.setHeader('Cache-Control', 'max-age=300, must-revalidate');
 
   const url = new URL(req.url || '/', 'http://localhost:' + PORT);
   const chunks = [];
   for await (const chunk of req) {
     chunks.push(chunk);
   }
-  const body = Buffer.concat(chunks).toString("utf-8")
-  
+  const body = Buffer.concat(chunks).toString('utf-8');
+
   console.log('[Request received]', req.method, url.pathname);
   console.log('[Request headers]', req.headers);
-  console.log('[Request body]', JSON.parse(body));
+  if (body != undefined && body != '') {
+    const safeParse = (() => {
+      try {
+        return JSON.parse(body);
+      } catch {
+        return undefined;
+      }
+    })();
+    if (safeParse) console.log('[Request body]', safeParse);
+  }
   
-  if (req.method === 'GET' && url.pathname === '/produtos') {
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    res.end();
+  } else if (req.method === 'GET' && url.pathname === '/') {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(`<html>
+      <head>
+        <title>Hello World!</title>
+      </head>
+      <body>
+        <h1>Hello World!</h1>
+      </body>
+    </html>`);
+  } else if (req.method === 'GET' && url.pathname === '/produtos') {
     res.statusCode = 200;
 
     const cor = url.searchParams.get('cor');
     const tamanho = url.searchParams.get('tamanho');
     console.log(url.searchParams, cor, tamanho);
 
-    res.end('GET!');
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ message: 'GET!' }));
   } else if (req.method === 'POST' && url.pathname === '/produtos') {
     res.statusCode = 201;
     res.end('POST!');
   } else if (req.method === 'POST' && url.pathname === '/login') {
     res.statusCode = 200;
+    // Cookie
+    res.setHeader(
+      'Set-Cookie',
+      'token=123; HttpOnly; SameSite=Strict; Max-Age=3600; Path=/; Secure;',
+    );
     res.end('LOGIN!');
   } else {
     res.statusCode = 404;
