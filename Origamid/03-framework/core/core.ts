29678ff2c347599000cec112fec07aaf01f84167
Otávio <otaviomendessantos2019@gmail.com>
2026-08-17 11:39:09 -0300

    course: 0304 Route Error

--- Origamid/03-framework/core/core.ts
+++ Origamid/03-framework/core/core.ts
@@ -4,6 +4,7 @@ import { Router } from './router.ts';
 import { customRequest } from './http/custom-request.ts';
 import { customResponse } from './http/custom-response.ts';
 import { bodyJson } from './middleware/body-json.ts';
+import { RouteError } from './utils/route-error.ts';
 
 export default class Core {
   PORT = 3000;
@@ -12,28 +13,48 @@ export default class Core {
 
   constructor() {
     this.router = new Router();
-    this.router.use([bodyJson])
+    this.router.use([bodyJson]);
     this.server = createServer(this.handler);
   }
 
   handler = async (request: IncomingMessage, response: ServerResponse) => {
-    const req = await customRequest(request);
-    const res = customResponse(response);
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    try {
+      const req = await customRequest(request);
+      const res = customResponse(response);
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.setHeader('Access-Control-Allow-Origin', '*');
 
-    for (const middleware of this.router.middlewares) {
-      await middleware(req, res)
+      for (const middleware of this.router.middlewares) {
+        await middleware(req, res);
+      }
+
+      const matched = this.router.find(req.method || '', req.pathname);
+      if (!matched) return res.status(404).end('Não encontrado');
+      const { route, params } = matched;
+      req.params = params;
+      for (const middleware of route.middlewares) {
+        await middleware(req, res);
+      }
+      await route.handler(req, res);
+    } catch (err) {
+      response.setHeader(
+        'Content-Type',
+        'application/problem+json; charset=utf-8',
+      );
+      let title = 'Um erro interno ocorreu.';
+      let status = 500;
+      if (err instanceof RouteError) {
+        console.error(
+          `[${request.method} ${request.url}] ${err.status} ${err.message}`,
+        );
+        status = err.status;
+        title = err.message;
+      } else {
+        console.error(err);
+      }
+      response.statusCode = status;
+      response.end(JSON.stringify({ message: title, status }));
     }
-    
-    const matched = this.router.find(req.method || '', req.pathname);
-    if (!matched) return res.status(404).end('Não encontrado');
-    const { route, params } = matched;
-    req.params = params;
-    for (const middleware of route.middlewares) {
-      await middleware(req, res);
-    }
-    await route.handler(req, res);
   };
 
   init = () => {
--- Origamid/03-framework/index.ts
+++ Origamid/03-framework/index.ts
@@ -1,6 +1,7 @@
 import Core from './core/core.ts';
 import db from './core/database.ts';
 import { logger } from './core/middleware/logger.ts';
+import { RouteError } from './core/utils/route-error.ts';
 
 const core = new Core();
 
@@ -13,6 +14,9 @@ core.router.get(
   '/curso/:slug',
   (req, res) => {
     console.log(req.params);
+    const { slug } = req.params;
+    if (slug !== 'javascript')
+      throw new RouteError(404, 'curso não encontrado');
     res.status(200).json({ message: 'hello world' });
   },
   [logger],
