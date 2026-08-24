e3df880fd6d6c1ac892ded584820d7862610992b
Otávio <otaviomendessantos2019@gmail.com>
2026-08-24 15:58:05 -0300

    course: 0502 Session Database

--- Origamid/05-autenticacao/api/auth/index.ts
+++ Origamid/05-autenticacao/api/auth/index.ts
@@ -1,10 +1,13 @@
 import { Api } from '../../core/utils/abstract.ts';
 import { RouteError } from '../../core/utils/route-error.ts';
 import { AuthQuery } from './query.ts';
+import { SessionService } from './services/session.ts';
 import { authTables } from './tables.ts';
 
 export default class authApi extends Api {
   query = new AuthQuery(this.db);
+
+  session = new SessionService(this.core);
 
   handlers = {
     postUser: (req, res) => {
@@ -21,20 +24,31 @@ export default class authApi extends Api {
         throw new RouteError(400, 'Erro ao criar usuário');
       res.status(201).json({ title: 'Usuário criado' });
     },
-    postLogin: (req, res) => {
+    postLogin: async (req, res) => {
       const { email, password } = req.body;
-      console.log('login', req.body);
-      const user = this.db.query(/*sql*/`
+
+      const user = this.db
+        .query(
+          /*sql*/ `
         SELECT "id", "password_hash"
         FROM users WHERE email = ?
-      `).get(email);
-      console.log('user', user);
+      `,
+        )
+        .get(email) as { id: number; password_hash: string } | undefined;
+
       if (!user || user.password_hash !== password) {
-        throw new RouteError(404, "Email ou senha incorretos");
+        throw new RouteError(404, 'Email ou senha incorretos');
       }
-      res.setHeader("Set-Cookie", `sid=${user.id}; Path=/;`)
+
+      const { sid_hash } = await this.session.create({
+        userId: user.id,
+        ip: req.ip,
+        ua: req.headers['user-agent'] ?? '',
+      });
+
+      res.setHeader('Set-Cookie', `sid=${sid_hash}; Path=/;`);
       res.status(200).json({ title: 'Login realizado' });
-    }
+    },
   } satisfies Api['handlers'];
 
   tables() {
@@ -43,6 +57,6 @@ export default class authApi extends Api {
 
   routes() {
     this.router.post('/auth/user', this.handlers.postUser);
-    this.router.post('/auth/login',this.handlers.postLogin);
+    this.router.post('/auth/login', this.handlers.postLogin);
   }
 }
---
+++ Origamid/05-autenticacao/api/auth/services/session.ts
@@ -0,0 +1,19 @@
+import { CoreProvider } from '../../../core/utils/abstract.ts';
+import { AuthQuery } from '../query.ts';
+
+export class SessionService extends CoreProvider {
+  query = new AuthQuery(this.db);
+
+  async create({ userId, ip, ua }: { userId: number; ip: string; ua: string }) {
+    const expires_ms = Date.now() + 60 * 60 * 24 * 15 * 1000; // 15 dias
+    const sid_hash = '1';
+    this.query.insertSession({
+      sid_hash,
+      user_id: userId,
+      ip,
+      ua,
+      expires_ms,
+    });
+    return { sid_hash };
+  }
+}
