a1b08fbd62a3dc18e52c9b4a891d565541092c33
Otávio <otaviomendessantos2019@gmail.com>
2026-09-01 15:06:33 -0300

    course: 0603 Formatar

--- Origamid/06_content-length/validate.ts
+++ Origamid/06_content-length/validate.ts
@@ -10,12 +10,41 @@ function number(x: unknown) {
 
 function boolean(x: unknown) {
   if (typeof x === 'boolean') return x;
-  if (x === true || x === 'true' || x === 1 || x === '1' || x === 'on') return true;
-  if (x === false || x === 'false' || x === 0 || x === '0' || x === 'off') return false;
+  if (x === true || x === 'true' || x === 1 || x === '1' || x === 'on')
+    return true;
+  if (x === false || x === 'false' || x === 0 || x === '0' || x === 'off')
+    return false;
   return undefined;
 }
 
-console.log(string(' a'));
-console.log(number(NaN));
-console.log(boolean('true'));
-console.log(boolean('false'));
+// console.log(string(' a'));
+// console.log(number(NaN));
+// console.log(boolean('true'));
+// console.log(boolean('false'));
+
+// formatar
+// normalizar
+// escapar
+// sanitizar 
+
+function cpf(x: string) {
+  return x.replace(/\D+/g, '');
+}
+
+cpf('123.456.789-09');
+
+const a = `é`.normalize('NFC');
+const b = `e\u0301`.normalize('NFC');
+
+console.log(a === b, a, b);
+
+const nome = ' Otávio '.trim();
+
+function escapeHtml(x: string) {
+  return x
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
+}
