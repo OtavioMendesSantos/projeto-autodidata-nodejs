763cbe43811ed07c9efca5cc5257e986778b13a6
Otávio <otaviomendessantos2019@gmail.com>
2026-09-10 17:09:59 -0300

    course: 0703 Transform 2

--- Origamid/07-stream/stream/transform.ts
+++ Origamid/07-stream/stream/transform.ts
@@ -5,18 +5,18 @@ import { createGzip } from 'node:zlib';
 
 const transform = new Transform({
   transform(chunk: Buffer, _enc, next) {
-    const text = chunk.toString().toUpperCase()
-    this.push(text)
-    console.log("=================================")
-    console.log(text.slice(0, 100))
-    next()
+    console.log(chunk.toString());
+    const dados = JSON.parse(chunk.toString());
+    const filtrados = dados.filter((item: any) => item.vitalicio === 'true');
+    this.push(JSON.stringify(filtrados));
+    next();
     // next(null, text);
   },
 });
 
 await pipeline(
-  createReadStream('./dados.json'),
-//   transform,
-  createGzip(), // transformador nativo do node
-  createWriteStream('./dados-saida.gz'),
+  createReadStream('./dados.json', { highWaterMark: 20 }),
+  transform,
+  // createGzip(), // transformador nativo do node
+  createWriteStream('./dados-saida.json'),
 );
