6f04166cfb5350e17afdf4875d9fe4e064b50b6c
Otávio <otaviomendessantos2019@gmail.com>
2026-09-10 16:14:12 -0300

    course: 0702 Stream 4

--- Origamid/07-stream/stream/server.ts
+++ Origamid/07-stream/stream/server.ts
@@ -1,12 +1,19 @@
-import { createWriteStream } from 'node:fs';
+import { createReadStream, createWriteStream } from 'node:fs';
 import { createServer } from 'node:http';
+import { pipeline } from 'node:stream/promises';
 
 const log = createWriteStream('./log.txt', { flags: 'a' });
 
-const server = createServer((req, res) => {
-  log.write(`${req.method} ${req.socket.remoteAddress}\n`);
-  res.write('pre teste ')
-  res.end('teste');
+const server = createServer(async (req, res) => {
+  try {
+    const dados = createReadStream('./dados.json');
+    log.write(`${req.method} ${req.socket.remoteAddress}\n`);
+    res.setHeader('content-type', 'application/json');
+    await pipeline(dados, res);
+  } catch (err) {
+    console.error(err);
+    res.end('error')
+  }
 });
 
 server.listen(3000).on('close', () => {
