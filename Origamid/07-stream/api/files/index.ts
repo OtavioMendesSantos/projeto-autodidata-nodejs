10fb789daaaab6cfea4e26d8e28abfd5f9ee0990
Otávio <otaviomendessantos2019@gmail.com>
2026-09-11 16:46:28 -0300

    course: 0704 Servir Arquivos 3

--- Origamid/07-stream/api/files/index.ts
+++ Origamid/07-stream/api/files/index.ts
@@ -1,13 +1,46 @@
-import { Api } from '../../core/utils/abstract.ts';
 import { createReadStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import path from 'node:path';
 import { pipeline } from 'node:stream/promises';
+import { Api } from '../../core/utils/abstract.ts';
+import { RouteError } from '../../core/utils/route-error.ts';
 import { v } from '../../core/utils/validate.ts';
+import { checkETag, mimeType } from './utils.ts';
 
 export default class filesApi extends Api {
   handlers = {
     sendFile: async (req, res) => {
       const name = v.file(req.params.name);
       const filePath = `./files/${name}`;
+      const ext = path.extname(name);
+      
+      let st;
+      try {
+        st = await stat(filePath);
+      } catch (e) {
+        throw new RouteError(404, 'Arquivo não encontrado');
+      }
+
+      const eTag = `W/${st.size.toString(16)}-${Math.floor(st.mtimeMs).toString(16)}`;
+
+      res.setHeader('ETag', eTag);
+      res.setHeader('X-Content-Type-Options', 'nosniff');
+      res.setHeader('Content-Length', `${st.size}`);
+      res.setHeader('Last-Modified', `${st.mtime.toUTCString()}`);
+      res.setHeader(
+        'Content-Type',
+        mimeType[ext] || 'application/octet-stream',
+      );
+      res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate');
+
+      if (checkETag(req.headers['if-none-match'], eTag)) {
+        res.status(304);
+        res.end();
+        console.log('cache :)');
+        return;
+      }
+      res.status(200);
+
       const file = createReadStream(filePath);
       await pipeline(file, res);
     },
