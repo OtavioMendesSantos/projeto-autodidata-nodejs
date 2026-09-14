a4b0b204f9f1b69cf6621c39982497d31db652cb
Otávio <otaviomendessantos2019@gmail.com>
2026-09-14 19:48:10 -0300

    course: 0811 Criar Curso

--- Origamid/08-front-end/api/lms/query.ts
+++ Origamid/08-front-end/api/lms/query.ts
@@ -43,9 +43,14 @@ export class LmsQuery extends Query {
     return this.db
       .query(
         /* sql */ `
-      INSERT OR IGNORE INTO "courses"
+      INSERT INTO "courses"
         ("slug","title","description","lessons","hours")
       VALUES (?,?,?,?,?)
+      ON CONFLICT ("slug") DO UPDATE SET
+        "title" = excluded."title",
+        "description" = excluded."description",
+        "lessons" = excluded."lessons",
+        "hours" = excluded."hours"
     `,
       )
       .run(slug, title, description, lessons, hours);
